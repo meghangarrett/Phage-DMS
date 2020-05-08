22a2a354ea2d45afe9940afd9ab5941a5c257abf
Kate Dusenbury <khdusenbury@gmail.com>
2020-05-07 20:33:59 -0700

    add oligo scripts and some gp120 fasta files

---
+++ library_design/README.md
@@ -0,0 +1,27 @@
+# Phage-DMS Oligos
+This repository contains scripts and sequences for making Phage-DMS libraries for the gp41, gp120, and V3 regions of HIV Env. Scripts were written by Kate H.D. Crawford and Jesse D. Bloom and experiment design - including sequence selection and optimization - was conducted by Meghan Garrett, Hannah Itell, and Julie Overbaugh.
+
+## Contents
+
+This directory contains the scripts and HIV Env sequences necessary for designing Phage-DMS libraries for gp41 and V3 from the BF520, BG505, and ZAA1197 strains and for gp120 from the BG505, B41, and DU422 strains. The wildtype sequences for these proteins are in the `sequences` directory. 
+
+## Making Phage-DMS libraries
+
+Phage-DMS libraries consist of oligos encoding overlapping peptides spanning the protein of interest where the middle residue for each oligo has been mutated from wildtype to encode any of the 20 amino acids.
+A complete library thereby consists of 20 oligos per site in the protein of interest. 
+
+The scripts contained in this repository create oligo libraries for the gp41, gp120, and V3 regioons of Env from several HIV strains.
+The oligos created by these scripts are 93 nucleotides long and each oligo advances by 3 nucleotides compared to the previous oligo. (These parameters are specified by the `oligo_length` and `tile` variables defined in the beginning of each script.)
+These scripts also add on the necessary adaptor sequences for cloning these oligos into the [T7 Select phage system](https://www.emdmillipore.com/US/en/product/T7Select10-3-Cloning-Kit,EMD_BIO-70550#anchor_USP).
+Additionally, user-specified linker sequences are added to the first and last oligos ordered to ensure the first and last sites to be mutated are in the middle of those oligos. 
+The scripts also use synonymous substitution to remove any sequences matching the restriction sites used for cloning. 
+For the T7 Select system, those restriction sites are `GAATTC` and `AAGCTT` matching the motifs for the EcoRI and HindIII restriction enzymes, respectively.
+The sequences to remove from the designed oligos are referred to as `avoid_motifs` and can be specified in the beginning of the scripts.
+Finally, since there is a lot of sequence similarity between the different HIV proteins for which we were designing oligos, the script removes all duplicate sequences, so each oligo ordered is unique.
+
+There are different scripts for each V3 library as each virus in this library had different linker sequences for the first and last oligos. The `gp41_oligos.py` script makes libraries for gp41 from the BG505, BF520, and ZAA1197 strains. The `gp120_oligos_181219.py` script makes Phage-DMS oligo libraries for gp120 from BG505, B41, and DU422. Of note, only the `gp120_oligos_181219.py` script allows for the input wildtype sequences to be different lengths.
+
+The output for each script is a `.csv` file with the columns `Virus`, `Rand_Loc`, `Rand_AA`, and `Oligo`. 
+The `Virus` column specifies the virus strain for which the oligos are designed; `Rand_Loc` specifies what residue is in the middle of the oligo and, thus, being randomized; `Rand_AA` specifies waht amino acid is encoded in this middle position; and `Oligo` is the final oligo seuqence including necessary adaptors.
+
+To run these scripts and re-make the oligos we designed, clone this repo and run each script from the command line (for example, use the command: `python gp41_oligos.py` to re-make the gp41 oligos). Python, BioPython, and Pandas are required for running these scripts.
---
+++ library_design/gp120_oligos_181219.py
@@ -0,0 +1,244 @@
+"""Script for making oligos for gp41 'mutational scan' phage display assay."""
+
+import Bio.Seq
+import Bio.SeqUtils.CodonUsageIndices
+import Bio.Alphabet.IUPAC
+import itertools
+import pandas
+import os
+
+from Bio.Seq import Seq
+from operator import itemgetter
+
+viruses = ['BG505', 'B41','DU422'] #List of file names for creating libraries
+oligo_length = 93
+tile = 3
+
+avoidmotifs = ['GAATTC', 'AAGCTT']
+
+linker5 = 'GGAGGAGGTGGTTCTGGTGGTGGAGGTTCAGGTGGTGGTGGAAGT' #These linker sequences have been updated to be less GC rich
+linker3 = 'GGAGGAGGTGGTTCTGGTGGTGGAGGTTCAGGTGGTGGTGGAAGT'
+adaptor5 = 'aggaattctacgctgagt' 
+adaptor3 = 'tgatagcaagcttgcc' 
+
+cai = Bio.SeqUtils.CodonUsageIndices.SharpEcoliIndex
+
+translation_table = {}
+codons = []
+amino_acids = []
+rt_table = {}
+
+for (_nt1, _nt2, _nt3) in itertools.product(Bio.Alphabet.IUPAC.IUPACUnambiguousDNA.letters, repeat = 3):
+    codon = _nt1 + _nt2 + _nt3
+    codons.append(codon)
+for codon in codons:
+    translation_table[codon] = str(Bio.Seq.Seq(codon).translate())
+    if translation_table[codon] not in amino_acids:
+        amino_acids.append(translation_table[codon])
+        rt_table[translation_table[codon]] = [codon]
+    else:
+        rt_table[translation_table[codon]].append(codon)
+
+score_dict = {} #Dict of all syn. codons & cai scores for an aa. No stop codons.
+for amino_acid in amino_acids:
+    if amino_acid != '*':
+        scores = []
+        for codon in rt_table[amino_acid]:
+            scores.append((codon, cai[codon]))
+        score_dict[amino_acid] = scores
+
+
+def translate(oligo):
+    """Script for translating DNA more quickly than Bio.Seq
+
+    Args:
+        `oligo` (str)
+            DNA sequence to translate
+
+    Returns:
+        `aa_read` (str)
+            Translated DNA sequence
+
+    >>> oligo = 'ATGGGC'
+    >>> translate(oligo) == 'MG'
+    True
+    """
+    assert len(oligo) % 3 == 0, "Not translatable due to length"
+    oligo = oligo.upper()
+    aa_read = ''.join([translation_table[oligo[i : i + 3]] for i in 
+            range(0, len(oligo), 3)])
+    return aa_read
+
+
+def get_seq(seq_file):
+    """Return sequence from a `.fasta` file `seq_file`
+    """
+    seq_by_line = []
+    with open(seq_file) as f:
+        for line in f:
+            if '>' in line:
+                continue # Can input sequence with fasta header
+            else:
+                seq = line.strip()
+                seq_by_line.append(seq)
+    seq = ''.join(seq_by_line)
+    return seq
+
+
+def subseq_rand_mid(full_seq):
+    """Subsequence viral seqs into oligos of *oligo_length* tiled by *tile*.
+
+    The middle amino acid in each oligo is randomized so that each region of 
+    gp41 is covered by 20 oligos each with a different amino acid at the 
+    middle location.
+
+    Args: 
+        `full_seq` (str)
+            Full DNA sequence (incl linkers) to be split up into oligos.
+
+    Returns:
+        `subseqs` (list)
+            List of subsequenced oligos with randomized middle amino acid.
+    """
+    subseqs = []
+    i = 0
+    while i <= (len(full_seq) - oligo_length):
+        subseq_front = full_seq[i : i + int(((oligo_length-3)/2))]
+        subseq_end = full_seq[i + int((((oligo_length-3)/2)+3)) : i + oligo_length]
+        for aa in amino_acids:
+            if aa != '*':
+                subseq_total = subseq_front + max(score_dict[aa], 
+                        key=lambda item:item[1])[0] + subseq_end
+                assert len(subseq_total) == oligo_length, 'Oligos not correct length'
+                subseqs.append(subseq_total)
+        i = i + tile
+    return subseqs
+    
+
+def remove_rsites(oligos, codon_scores_by_aa):
+    """Remove restriction sites from subsequenced oligos.
+
+    Args:
+        `oligos` (list)
+            List of oligos. Will remove rsites from each oligo.
+
+        `codon_scores_by_aa` (dict)
+            Dictionary of all codons and cai scores for each amino acid.
+                e.g. {'C': [('TGT', 0.5), ('TGC', 1)]}
+    
+    Returns:
+        `oligos` (list)
+            Returns the list of oligos with restriciton sites removed. 
+    """
+    rsites_count = 0
+    replace = False
+    for motif in avoidmotifs:
+        for n in range(len(oligos)):
+            oligos[n] = oligos[n].upper()
+            if motif in oligos[n]:
+                aa_withrs = translate(oligos[n])
+                replace = True
+                while replace:
+                    for i in range(len(oligos[n]) - len(motif) + 1):
+                        seq = oligos[n][i : i + len(motif)] # sequence starting at i
+                        if seq == motif:
+                            done = False #need to replace a codon in this seq
+                            rsites_count += 1
+                            if oligos[n][i-(i%3):i-(i%3)+3] == 'ATG' or oligos[n][i-(i%3):i-(i%3)+3] == 'TGG': #Make sure codon to replace has synonymous codons
+                                j = i + 3
+                                print('Codon to replace ({0}) has no synonymous codons. Replace next codon: {1}'). \
+                                        format(oligos[n][i-(i%3):i-(i%3)+3], oligos[n][j-(j%3):j-(j%3)+3])
+                                i = j
+                            for r in codon_scores_by_aa: #For each amino acid...
+                                for codon_tup in codon_scores_by_aa[r]: #...And for each (codon, score) tuple that encodes that aa...
+                                    if not done: #If codon at this location has not been replaced
+                                        if oligos[n][i-(i%3):i-(i%3)+3] in codon_tup: #If codon we want to replace (first codon that contains part of restriciton site) is in score tuple codon_tup...
+                                            l = codon_scores_by_aa[r] #...Turn all synonymous codons and their scores into a list
+                                            l.sort(key=itemgetter(1), 
+                                                    reverse=True)
+                                            if oligos[n][i-(i%3):i-(i%3)+3] == l[0][0]:
+                                                print('Replacing highest scoring codon ({0}) with '\
+                                                        '2nd highest scoring: {1}'.format(l[0][0], l[1][0]))
+                                                new_codon = l[1][0]
+                                            elif oligos[n][i-(i%3):i-(i%3)+3] != l[0][0]:
+                                                print('Replacing codon ({0}) with highest scoring codon: {1}'.\
+                                                        format(oligos[n][i-(i%3):i-(i%3)+3], l[0][0]))
+                                                new_codon = l[0][0]
+                                            clean_oligo = oligos[n][:i-(i%3)] \
+                                                    + new_codon \
+                                                    + oligos[n][i-(i%3)+3:]
+                                            oligos[n] = clean_oligo
+                                            done = True #Codon has been replaced, don't look at other possible synonymous codons.
+                    if motif not in oligos[n]: #Make sure motif not in oligo after going through replacements
+                        replace = False
+                assert len(oligos[n]) == oligo_length, 'Oligo lengths not maintained ' \
+                        'while removing restriction sites.'          
+                assert aa_withrs == translate(oligos[n]), 'The amino acid sequence ' \
+                        'has been altered by removing restriction site.'
+    print('{0} restriction sites were removed with synonymous substitution.'
+            .format(rsites_count))
+    return oligos
+
+
+def main():
+
+    virus_oligos = {}
+
+    for virus in viruses:
+        print('\nDesigning oligos for {0}.'.format(virus))
+        opt_seq_file = 'sequences/{0}_gp120_dna_opt.fasta'.format(virus)
+        opt_dna_seq = get_seq(opt_seq_file)
+
+        assert translate(opt_dna_seq).upper() == get_seq('sequences/{0}_gp120_prot.fasta'
+                .format(virus)).upper(), 'Translated seq does not equal given protein sequence.'
+
+        full_seq = linker5 + opt_dna_seq + linker3
+        assert len(full_seq) % 3 == 0, "Adding linkers made sequence untranslatable"
+        
+        subsequences = subseq_rand_mid(full_seq)
+
+        cleaned_oligos = remove_rsites(subsequences, score_dict)
+
+        oligos_with_translation = []
+        oligo_count = 0
+        for oligo in cleaned_oligos:
+            translated_oligo = translate(oligo)
+            random_aa = translated_oligo[int((len(translated_oligo)-1)/2)]
+            final_oligo = adaptor5 + oligo + adaptor3
+            oligos_with_translation.append((final_oligo, translated_oligo, random_aa, oligo_count))
+            oligo_count += 1 #Keep track of oligo number separately for each virus
+        
+        virus_oligos[virus] = oligos_with_translation
+
+    all_oligos_count = 0
+    for virusID in virus_oligos:
+        virus_oligo_num = len(virus_oligos[virusID])
+        print('Designed {0} oligos for {1}.'.format(virus_oligo_num, virusID))
+        all_oligos_count += virus_oligo_num
+
+    all_oligos_df = pandas.melt(pandas.DataFrame(dict([(k, pandas.Series(v)) for k,v in virus_oligos.items()])), 
+            value_vars=viruses, var_name='Virus', value_name='Oligo_Info')
+
+    all_oligos_df = all_oligos_df.dropna()
+
+    assert all_oligos_count == len(all_oligos_df), 'Incorrect number of oligos designed after converting to DataFrame.'
+
+    all_oligos_df[['Oligo', 'Prot', 'Rand_AA', 'Count']] = all_oligos_df['Oligo_Info'].apply(pandas.Series)
+    print('{0} oligos designed.'.format(len(all_oligos_df)))
+    all_oligos_df = all_oligos_df.drop(['Oligo_Info'], axis=1).drop_duplicates(['Prot'], keep='first')
+    print('{0} oligos remain after dropping duplicate protein sequences.'.format(len(all_oligos_df)))
+
+    all_oligos_df['Rand_Loc'] = all_oligos_df['Count'] // 20 + 1
+    all_oligos_df = all_oligos_df.reset_index(drop=True)
+
+    if not os.path.isdir('./oligos'):
+        os.mkdir('./oligos')
+        
+    all_oligos_df.to_csv('./oligos/gp120_master_oligos.txt', index_label='Oligo_Num', 
+            columns=['Virus', 'Rand_Loc', 'Rand_AA', 'Oligo'])
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    main()
